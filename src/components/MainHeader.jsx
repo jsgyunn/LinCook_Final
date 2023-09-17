f73aec708cf40eda0a221bf1326bb129edb28b26
jsgyunn <jsg1623@gmail.com>
2023-09-17 19:26:48 +0900

    update Alert, Main

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,56 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import mainlogo from '../assets/mainlogo.png'
+import Alert from './Alert'
 import registration from '../assets/registrationlogo.png'
 import CookingRegistration1 from '../pages/CookingRegistration1'
 
 
 
+
 export default function MainHeader() {
 
+    const [showAlert, setShowAlert] = useState(false);
 
+    const handleButtonClick = () => {
+        setShowAlert(true);
+    }
 
 
 
     return (
-        <main className="relative h-[70vh] overflow-hidden bg-white dark:bg-gray-300">
-            <header className="z-30 flex items-center w-full h-24 sm:h-15 ">
-                <div className="container flex items-center justify-between px-6 mx-auto">
-                </div>
-            </header>
-            <div className="relative z-20  flex items-center overflow-hidden bg-white dark:bg-gray-300">
-                <div className="container relative flex px-5 py-30 mx-auto">
-                    <div className="relative z-20 flex flex-col sm:w-2/3 lg:w-2/5">
-                        <h1 className="flex flex-col text-6xl font-black leading-none uppercase font-bebas-neue sm:text-8xl dark:text-white">
-                            Click
-                            <span className="text-5xl sm:text-7xl">
-                                the Link !
-                            </span>
-                        </h1>
-                        <p className="text-3xl font-bold dark:text-gray-800 mt-5">
+        <section
+            className="relative bg-[url(https://images.unsplash.com/photo-1505935428862-770b6f24f629?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2934&q=80)] 
+            bg-cover bg-center bg-no-repeat "
+        >
+            <div
+                className="absolute inset-0 backdrop-blur-md bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l backdrop-opacity-10"
+            ></div>
+
+            <div
+                className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+            >
+
+
+                <div className="max-w-xl mx-auto text-center ltr:sm:text-left rtl:sm:text-right">
+                    <h1 className="text-3xl font-extrabold sm:text-5xl">
+                        Click the Link!
+
+                        <strong className="block font-extrabold text-green-600">
                             간편한 장보기, 링쿡하세요.
-                        </p>
-                        {/* <div className="flex mt-8">
-                            <a href="#" className="px-4 py-2 mr-4 text-white uppercase bg-pink-500 border-2 border-transparent rounded-lg text-md hover:bg-pink-400">
-                                Get started
-                            </a>
-                            <a href="#" className="px-4 py-2 text-pink-500 uppercase bg-transparent border-2 border-pink-500 rounded-lg dark:text-white hover:bg-pink-500 hover:text-white text-md">
-                                Read more
-                            </a>
-                        </div> */}
+                        </strong>
+                    </h1>
+
+                    <p className="mt-6 sm:mt-8 text-base sm:text-xl/relaxed font-bold">
+                        링쿡은 지금 보고 있는 요리 영상에 필요한 재료들을 빠르게 찾아주고, <br /> 편리한 서비스로 당신의 요리 경험을 더욱 완벽하게 만들어드립니다!
+                    </p>
+
+                    <div className="mt-8 flex flex-wrap gap-4 items-center justify-center">
+                        <input
+                            type="url"
+                            placeholder="YouTube Link"
+                            className="w-full sm:w-96 rounded-md bg-white p-3 text-gray-700 shadow-sm transition focus:border-white focus:outline-none focus:ring focus:ring-green-500"
+                        />
+
+                        <button
+                            type="submit"
+                            className="group flex items-center justify-center gap-2 rounded-md bg-green-600 px-5 py-3 text-white transition sm:w-auto hover:bg-green-700"
+                            onClick={handleButtonClick}
+                        >
+                            <span className="text-sm font-medium"> 적용 </span>
+                        </button>
+                        {showAlert && <Alert />}
                     </div>
-                    <div className="relative hidden sm:block sm:w-1/3 lg:w-3/5">
+
+                    <div className="relative hidden sm:block sm:w-1/3 lg:w-3/5 mt-5 mx-auto">
                         <Link to='/cookingregistration1'>
-                            <img src={registration}
-                                className="w-full max-w-xs m-auto md:max-w-xl" />
+                            <img src={registration} className="w-full max-w-xs m-auto md:max-w-xl" />
                         </Link>
                     </div>
                 </div>
+
+
+
+
+
             </div>
-        </main >
+        </section>
 
     )
 }
-
-
-
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import CookingRegistrationCard from '../components/CookingRegistrationCard'
+import Alert from '../components/Alert'
 
 export default function Test() {
 
     return (
-        <div className="grid grid-rows-3 grid-flow-col gap-4">
-            <div className="row-start-2 row-span-2 bg-gray-500">01</div>
-            <div className="row-end-3 row-span-2 bg-gray-500">02</div>
-            <div className=" mr-4 h-80 row-start-1 row-end-4 bg-gray-500">03</div>
-        </div>        // <CookingRegistrationCard />
+        <Alert />
     )
 }
